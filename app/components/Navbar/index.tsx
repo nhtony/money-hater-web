833ce4f3af81c9127e5e2fd0157f2d2753404a5d
nhtony <honhathao062@gmail.com>
2024-06-22 14:10:35 +0700

    Add shadow for navbar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -3,8 +3,8 @@ import { IconPlus } from "..";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-center items-center fixed inset-x-0 bottom-0 max-h-16 h-16 bg-white">
-      <div className="flex justify-center items-center bg-pink-500 rounded-full w-9 h-9">
+    <div className="flex justify-center items-center fixed inset-x-0 bottom-0 max-h-16 h-16 bg-white shadow-2xl rounded-md">
+      <div className="flex justify-center items-center bg-pink-500 rounded-full w-9 h-9 shadow-2xl">
         <IconPlus />
       </div>
     </div>
